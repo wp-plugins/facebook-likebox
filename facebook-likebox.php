7dd2a58b87985c93051113773e96b7a954c8d73c
contact-banker <contact-banker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-18 04:00:41 +0000

    New Version Release
    git-svn-id: https://plugins.svn.wordpress.org/facebook-likebox/trunk@1162418 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- facebook-likebox.php
+++ facebook-likebox.php
@@ -4,7 +4,7 @@ Plugin Name: Facebook Likebox
 Plugin URI: http://tech-banker.com
 Description: Let people share pages and content from your site back to their Facebook profile with one click, so all their friends can read them.
 Author: Tech Banker
-Version: 1.0.6
+Version: 1.0.7
 Author URI: http://tech-banker.com
 License: GPLv3 or later
 */
