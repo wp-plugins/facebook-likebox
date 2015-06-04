857ae4b48c7ab52a9b83b13afc1372a0aa7586e3
contact-banker <contact-banker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-04 04:23:53 +0000

    New Version Release
    git-svn-id: https://plugins.svn.wordpress.org/facebook-likebox/tags/1.0.8@1174123 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/top-bar-menus.php
@@ -0,0 +1,48 @@
+<?php
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////								Creating Top-bar Menus								///////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+if (!is_user_logged_in())
+{
+	return;
+}
+else
+{
+	$wp_admin_bar->add_menu(array(
+		"id" => "facebook_likebox",
+		"title" => "<img src=\"".plugins_url("/assets/images/icon.png",dirname(__FILE__))."\" width=\"25\" height=\"25\" style=\"vertical-align:text-top; margin-right:5px;\" />Facebook Likebox" ,
+		"href" => site_url()."/wp-admin/admin.php?page=facebook_likebox",
+	));
+	$wp_admin_bar->add_menu(array(
+		"parent" => "facebook_likebox",
+		"id" => "facebook_pages",
+		"href" => site_url()."/wp-admin/admin.php?page=facebook_likebox",
+		"title" => __("Facebook Pages",facebook_likebox))									/* set the sub-menu name */
+	);
+	$wp_admin_bar->add_menu(array(
+		"parent" => "facebook_likebox",
+		"id" => "system_status",
+		"href" => site_url()."/wp-admin/admin.php?page=fblb_system_status",
+		"title" => __("System Status",facebook_likebox))									/* set the sub-menu name */
+	);
+	$wp_admin_bar->add_menu(array(
+		"parent" => "facebook_likebox",
+		"id" => "general_settings",
+		"href" => site_url()."/wp-admin/admin.php?page=fblb_general_Settings",
+		"title" => __("General Settings",facebook_likebox))									/* set the sub-menu name */
+	);
+	$wp_admin_bar->add_menu(array(
+		"parent" => "facebook_likebox",
+		"id" => "recommendations",
+		"href" => site_url()."/wp-admin/admin.php?page=fblb_recommendations",
+		"title" => __("Recommendations", facebook_likebox))									/* set the sub-menu name */
+	);
+	$wp_admin_bar->add_menu(array(
+		"parent" => "facebook_likebox",
+		"id" => "our_other_services",
+		"href" => site_url()."/wp-admin/admin.php?page=fblb_our_other_services",
+		"title" => __("Our Other Services", facebook_likebox))								/* set the sub-menu name */
+	);
+}
+?>
